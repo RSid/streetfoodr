6f0da3ff20feabe0fc1d9ac56d1b35992f103307
RSid <sm1th303@gmail.com>
2015-01-11 16:18:23 -0500

    test for throwing 404 when asked for city schedule for unavailable city

--- spec/foodtrucks_spec.rb
+++ spec/foodtrucks_spec.rb
@@ -8,4 +8,9 @@ describe "#foodtrucks city schedule" do
     toronto_foodtrucks_data = FoodTruck.city_schedule("toronto")
     expect(boston_foodtrucks_data.length).to be > 0
   end
+
+  it 'throws a useful error when the city does not have data' do
+    expect{ FoodTruck.city_schedule("thimphu") }
+      .to raise_error(RestClient::ResourceNotFound)
+  end
 end
