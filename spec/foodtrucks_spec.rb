1333766ab187817c2ddce1644d7a54c6dc5f2a1a
RSid <sm1th303@gmail.com>
2015-01-11 15:54:08 -0500

    added another part to test to ensure more cities than boston work

--- spec/foodtrucks_spec.rb
+++ spec/foodtrucks_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper.rb'
 
-describe "#foodtrucks" do
+describe "#foodtrucks city schedule" do
   it 'gets all food truck data for a given city' do
     boston_foodtrucks_data = FoodTruck.city_schedule("boston")
+    expect(boston_foodtrucks_data.length).to be > 0
+
+    toronto_foodtrucks_data = FoodTruck.city_schedule("toronto")
     expect(boston_foodtrucks_data.length).to be > 0
   end
 end
